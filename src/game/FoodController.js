6bdf30492ba4c3cbf2a4dd916f5dbbe6b88121ab
Jon Uy <jonxuy@gmail.com>
2015-08-06 15:28:23 -0400

    Food assets + initial spawn of food with gravity

--- src/game/FoodController.js
+++ src/game/FoodController.js
@@ -1,9 +1,75 @@
 function FoodController() {
-  function update() {
+  var foodLibrary;
+  var currFoods;
+  var startTime;
+  var lastSpawnTime;
 
+  // Spawn delay in milliseconds
+  var minSpawnDelay = 500;
+  var maxSpawnDelay = 2000;
+  var maxFoodAllowed = 5;
+
+  function init() {
+    currFoods = [];
+    foodLibrary = [];
+
+    lastSpawnTime = 0;
+  }
+
+  function preload() {
+    var tmp;
+    
+    tmp = {name: 'food1', image: game.load.image('food1', 'assets/food1.png')};
+    foodLibrary[foodLibrary.length] = tmp;
+    tmp = {name: 'food2', image: game.load.image('food1', 'assets/food2.png')};
+    foodLibrary[foodLibrary.length] = tmp;
+    tmp = {name: 'food3', image: game.load.image('food1', 'assets/food3.png')};
+    foodLibrary[foodLibrary.length] = tmp;
+  }
+
+  function startSpawn() {
+    var d = new Date();
+    startTime = d.getTime();
+  }
+
+  function update() {
+    var currTime;
+
+    currTime = (new Date()).getTime();
+
+    // @todo destroy foods when they fall out of frame
+    // @todo spawn new foods when able
+    // if (currFoods.length < maxFoods) {
+    if (currTime - lastSpawnTime > 1000) {
+      spawnFood();
+    }
+  }
+
+  function spawnFood() {
+    var x;
+    var y;
+    var sprite;
+
+    x = Math.floor(Math.random() * 700);
+    x += 50;
+    y = 0;
+
+    // Create and position sprite
+    sprite = game.add.sprite(x, y, foodLibrary[0].name);
+
+    // Add physics to the sprite
+    game.physics.enable([sprite], Phaser.Physics.ARCADE);
+    sprite.body.gravity.y = 150;
+
+    currFoods[currFoods.length] = sprite;
+
+    lastSpawnTime = (new Date()).getTime();
   }
 
   return {
+    init: init,
+    preload: preload,
+    startSpawn: startSpawn,
     update: update
   }  
 }
--- src/game/GameState.js
+++ src/game/GameState.js
@@ -12,7 +12,7 @@ function GameState() {
    */
   function init() {
     // @todo placeholder
-    var text = "baby-burper-2l15";
+    var text = "baby-burper-2k15";
     var style = { font: "24px Arial", fill: "#fff", align: "center" };
     var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
     t.anchor.setTo(0.5, 0.5);
@@ -21,6 +21,8 @@ function GameState() {
     babyController = new BabyController();
     foodController = new FoodController();
     burpController = new BurpController();
+
+    foodController.init();
 
     gameController.setBaby(babyController);
     gameController.setFood(foodController);
@@ -31,14 +33,14 @@ function GameState() {
    * preload()
    */
   function preload() {
-
+    foodController.preload();
   }
 
   /**
    * create()
    */
   function create() {
-
+    foodController.startSpawn();
   }
 
   /**
@@ -46,6 +48,7 @@ function GameState() {
    */
   function update() {
     gameController.update();
+    foodController.update();
   }
 
   return {
